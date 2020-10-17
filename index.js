8788c6a1cd5802203f8e7924d8d6de350857f8b5
Mandar <mkmandar123@gmail.com>
2020-10-17 21:25:34 +0530

    fixed port issue

--- index.js
+++ index.js
@@ -49,6 +49,6 @@ app.get('/', (req, res) => {
     res.status(200).json({ success: true, message: 'Successfully setup' });
 });
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log('Server started on 3000');
 });

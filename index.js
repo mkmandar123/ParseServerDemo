3ada550f1c3c2ee83326d27240d90481529b330a
Mandar <mkmandar123@gmail.com>
2020-10-17 21:23:35 +0530

    fixed url

--- index.js
+++ index.js
@@ -5,26 +5,26 @@ const ParseDashboard = require('parse-dashboard');
 const app = express();
 
 const api = new ParseServer({
-    databaseURI: 'mongodb://localhost:27017/parse-test', // Connection string for your MongoDB database
+    databaseURI: 'mongodb+srv://me:dcbdwK45ZsusXqRO@cluster0.1inoe.mongodb.net/testParse?retryWrites=true&w=majority', // Connection string for your MongoDB database
     // cloud: '/home/myApp/cloud/main.js', // Absolute path to your Cloud Code
     appId: 'myAppId',
     masterKey: 'myMasterKey', // Keep this key secret!
     fileKey: 'optionalFileKey',
-    serverURL: 'http://localhost:3000/parse', // Don't forget to change to https if needed
+    serverURL: 'https://mandar-parse-server.herokuapp.com/parse', // Don't forget to change to https if needed
     javaScriptKey: 'myJSKey'
 });
 
 
 Parse.initialize('myAppId', 'myJSKey');
 Parse.masterKey = 'myMasterKey';
-Parse.serverURL = 'http://localhost:3000/parse';
+Parse.serverURL = 'https://mandar-parse-server.herokuapp.com/parse';
 
 app.use('/parse', api);
 
 const dashboard = new ParseDashboard({
     "apps": [
         {
-            "serverURL": "http://localhost:3000/parse",
+            "serverURL": "https://mandar-parse-server.herokuapp.com/parse",
             "appId": "myAppId",
             "masterKey": "myMasterKey",
             "appName": "Testing Parse Server"
